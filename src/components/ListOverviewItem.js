dfaf20fb0f1292b32c6c65cf5d5e566a193a3a49
PiIsFour <PiIsFour@users.noreply.github.com>
2020-04-09 19:15:20 +0200

    add ListOverviewItem as droptarget for todoItems

--- src/components/ListOverviewItem.js
+++ src/components/ListOverviewItem.js
@@ -1,10 +1,30 @@
 import React from 'react'
+import { useDrop } from 'react-dnd'
+import { useDispatch } from 'react-redux'
+import * as R from 'ramda'
 
-const ListOverviewItem = ({id, name, isSelected, onSelect}) => {
+import { DragItemType, moveTodoToList } from '../actions/dndActions'
+
+const ListOverviewItemView = ({id, name, dropRef, isSelected, onSelect}) => {
 	return <li
 		className={isSelected ? 'active' : ''}
+		ref={dropRef}
 		onClick={() => onSelect(id, name)}
 	>{name}</li>
+}
+
+const ListOverviewItem = ({id, name, isSelected, onSelect}) => {
+	const dispatch = useDispatch()
+	const dispatchMoveItemToList = R.compose(dispatch, moveTodoToList)
+	const [, drop] = useDrop({
+		accept: DragItemType.todoItem,
+		drop: (item) => dispatchMoveItemToList(item, {id, name}),
+	})
+	return <ListOverviewItemView id={id} name={name}
+		dropRef={drop}
+		isSelected={isSelected}
+		onSelect={onSelect}
+	/>
 }
 
 export default ListOverviewItem
